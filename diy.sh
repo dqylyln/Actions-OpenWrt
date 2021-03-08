29d3266df622cae3a1d7753bc9984cf9a0cf4bfe
DongQiYu <dqylyln@163.com>
2021-03-08 22:25:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -23,8 +23,6 @@ git clone https://github.com/zzsj0928/luci-app-serverchand package/luci-app-serv
 ### fix shadowsocksr-libev which does not exist
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-vssr/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-vssr/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 #### update and install feeds
 ./scripts/feeds update -a
