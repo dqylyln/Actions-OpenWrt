4d75370b5d0474d37ee5a7e385fe10604daf2e37
DongQiYu <dqylyln@163.com>
2021-03-22 20:24:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,20 +10,26 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 #### add ext luci-app
 git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-#### passwall depend
+#### ext passwall depend
 git clone https://github.com/kenzok8/small package/small
+
+#### remove ext for customize import conflict plugins
+rm -rf package/openwrt-packages/luci-app-ssr-plus
+rm -rf package/openwrt-packages/luci-app-jd-dailybonus
+
+####================= customize import plugins =================############
+#### hello world
+git clone https://github.com/fw876/helloworld package/helloworld
 ### serverchan WX
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 ### serverchan dingTalk
 git clone https://github.com/zzsj0928/luci-app-serverchand package/luci-app-serverchand
+### jd-dailybonus
+git cllone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
 #### add theme
 #git clone https://github.com/openwrt-develop/luci-theme-atmaterial package/luci-theme-atmaterial
 #git clone https://github.com/apollo-ng/luci-theme-darkmatter.git
-
-### bypass shadowsocksr-libev which does not exist
-#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-#find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
 #### update and install feeds
 ./scripts/feeds update -a
